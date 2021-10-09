860592e4830f05a3ecfb34ef081d60b83a035657
윤지인 <74345861+22000487zixxin@users.noreply.github.com>
2021-10-09 21:15:29 +0900

    solve errors !!!!!!

--- src/com/todo/menu/Menu.java
+++ src/com/todo/menu/Menu.java
@@ -10,7 +10,7 @@ public class Menu {
         System.out.println("[edit] 항목 수정");
         System.out.println("[ls] 전체 목록");
         System.out.println("[find (키워드)] 항목 검색");
-        System.out.println("[find_cate (키워드)] 키워드 검색");
+        System.out.println("[find_cate (키워드)] 카테고리 검색");
         System.out.println("[ls_name] 제목순 정렬");
         System.out.println("[ls_name_desc] 제목역순 정렬");
         System.out.println("[ls_date] 날짜순 정렬");
